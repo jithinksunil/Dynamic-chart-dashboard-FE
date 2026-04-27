73317c33930b6358cbb15a1b75bfe205f22259a0
Jithin K Sunil <jithinksunil96@gmail.com>
2026-04-27 18:29:19 +0530

    feat: integrate Prettier and ESLint for code formatting and linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,9 +16,16 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
+    plugins: {
+      prettier,
+    },
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'error',
     },
   },
 ])
